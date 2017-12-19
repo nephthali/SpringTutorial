e20d05c6f87f8652078a6d9628bd7c970831ee9b
nephthali <n.djabontchounda@cineca.it>
2017-12-19 16:26:52 +0100

    Reset of master step 1

--- coreContainer/src/main/java/org/ennva/spring_maven_tutorial/App.java
+++ coreContainer/src/main/java/org/ennva/spring_maven_tutorial/App.java
@@ -4,10 +4,36 @@ package org.ennva.spring_maven_tutorial;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /**
+         * Spring Have 2 type of container:
+         * 1- First Spring Container (BeanFactory)
+         *      This first one have a method XmlBeanFactory deprecated as of Spring 3.1
+         *      in in favor of DefaultListableBeanFactory and XmlBeanDefinitionReader
+         *
+         * 2- Second Spring container (ApplicationContext)
+         *      This is a second That is build on top of BeanFactory
+         *      and Most use as of Spring 3.1 **/
+//      Resource resource = new ClassPathResource("beans.xml");
+//      BeanFactory factory = new XmlBeanFactory(resource);
+//      Employee emp1 = (Employee)factory.getBean("emp1");
+//      Employee emp2 = factory.getBean("emp2",Employee.class);
+
+        // Second Spring Container
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Employee emp1 = (Employee)context.getBean("emp1");
+        Employee emp2 = context.getBean("emp2",Employee.class);
+
+        System.out.println("Employee Details" + emp1);
+        System.out.println("Employee Details" + emp2);
+
+        // Destroy the Bean Object :
+        //You have to reference the ClassPathXmlApplicationContext
+        ClassPathXmlApplicationContext ctx = (ClassPathXmlApplicationContext)context;
+        // And close the container
+        ctx.close();
     }
 }
