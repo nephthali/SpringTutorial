6b7c882acf39c39cde4e338cee8185dedb1b2d2e
nephthali <n.djabontchounda@cineca.it>
2017-12-19 16:58:51 +0100

    Reset from step1 branch

--- src/main/java/org/ennva/spring_maven_tutorial/App.java
+++ src/main/java/org/ennva/spring_maven_tutorial/App.java
@@ -1,7 +1,5 @@
 package org.ennva.spring_maven_tutorial;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * First java App entry class!
